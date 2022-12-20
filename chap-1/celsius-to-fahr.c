165cf8143b19571afb9b35cb6aae7e93aee08d47
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-20 19:08:59 -0300

    define statements

--- chap-1/celsius-to-fahr.c
+++ chap-1/celsius-to-fahr.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 100
+#define STEP  10
+
 void main() {
   int celsius;
 
   printf("%6s\t%3s\n", "Celsius", "Fahrenheit");
-  for (celsius=100; celsius > 0; celsius=celsius-10)
+  for (celsius= UPPER; celsius > LOWER; celsius = celsius - STEP)
     printf("%3d %6.1f\n", celsius, (celsius * (9.0/5.0)) + 32);
 
 }
