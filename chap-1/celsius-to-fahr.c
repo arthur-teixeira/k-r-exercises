f6f2f976c4ed25b6d9c600cbb0c9c9755ec5177d
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-20 19:06:25 -0300

    Prints table in reverse order

--- chap-1/celsius-to-fahr.c
+++ chap-1/celsius-to-fahr.c
@@ -4,7 +4,7 @@ void main() {
   int celsius;
 
   printf("%6s\t%3s\n", "Celsius", "Fahrenheit");
-  for (celsius=0; celsius <= 100; celsius=celsius+10)
+  for (celsius=100; celsius > 0; celsius=celsius-10)
     printf("%3d %6.1f\n", celsius, (celsius * (9.0/5.0)) + 32);
 
 }
