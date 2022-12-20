9eaefda80a3a8b389a3067ac2f2d7e99b9e3ec02
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-20 18:44:38 -0300

    Divide by float fraction (no truncation)

--- chap-1/fahr-to-celsius.c
+++ chap-1/fahr-to-celsius.c
@@ -9,7 +9,7 @@ void main() {
 
   float fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
+    celsius = (fahr-32) * (5.0/9.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
