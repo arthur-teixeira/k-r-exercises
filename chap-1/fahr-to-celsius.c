e06f415615db26c9b780a07937dede12109ed65a
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-20 18:55:52 -0300

    convert to for loop

--- chap-1/fahr-to-celsius.c
+++ chap-1/fahr-to-celsius.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 
 void main() {
-  float celsius;
-
-  float lower = 0;
-  float upper = 300;
-  float step = 20;
-
-  float fahr = lower;
+  int fahr;
 
   printf("F°\tC°\n");
-  while (fahr <= upper) {
-    celsius = (fahr-32) * (5.0/9.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
-  }
+  for (fahr=0; fahr <= 300; fahr = fahr + 20)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32)) ;
 }
