74e6a6607d03dab80145189017cf3c46221383c3
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-20 18:37:55 -0300

    declare and define variables together

--- chap-1/fahr-to-celsius.c
+++ chap-1/fahr-to-celsius.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 void main() {
-  int fahr, celsius;
-  int lower, upper, step;
+  int celsius;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+  int lower = 0;
+  int upper = 300;
+  int step = 20;
 
-  fahr = lower;
+  int fahr = lower;
   while (fahr <= upper) {
     celsius = 5 * (fahr-32) / 9;
     printf("%d\t%d\n", fahr, celsius);
