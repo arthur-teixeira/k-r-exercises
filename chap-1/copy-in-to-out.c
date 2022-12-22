9858a34d0f8099828daea5d1b902f351e8dda1bf
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-22 19:23:50 -0300

    Exercise 1-9 with an FSM

---
+++ chap-1/copy-in-to-out.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Write a program that copies its input to its output, replacing multiple blanks with a single blank
+
+// Finite State Machine
+// if char is blank: getchar until it is not blank, print blank
+// else: print char
+
+void main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') { 
+      while ((c = getchar()) == ' ')
+        ;
+      putchar(' ');
+      if (c == EOF) 
+        break;
+    }
+    putchar(c);
+  }   
+}
