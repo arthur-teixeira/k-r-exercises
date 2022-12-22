306c2d94e5dcb355cb0c6c07995fef134eb89cfa
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-22 19:08:05 -0300

    program that counts blanks, newLines and tabs

---
+++ chap-1/count-blanks.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main() {
+  long blanks = 0;
+  long tabs = 0;
+  long newLines = 0;
+
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\n')
+      ++newLines;
+    else if (c == '\t')
+      ++tabs;
+    else if (c == ' ')
+      ++blanks;
+  }
+
+  printf("%1d blanks\n", blanks);
+  printf("%1d tabs\n", tabs);
+  printf("%1d newLines\n", newLines);
+
+}
