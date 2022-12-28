fa9a7e8f9f80fee4abeceae4b3c745102f133123
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-28 20:30:51 -0300

    Print longest line

---
+++ chap-1/print-longest-line.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int get_next_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+  int len, max;
+  char line[MAXLINE];
+  char longest[MAXLINE];
+
+  max = 0;
+  while((len = get_next_line(line, MAXLINE)) > 0)
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  if (max > 0)
+    printf("%s\n", longest);
+
+  return 0;
+}
+
+int get_next_line(char s[], int lim) {
+  int c, i;
+
+  i = 0;
+  while (i < lim - 1) {
+    c = getchar();
+    if (c == EOF || c == '\n') break;
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+
+  return i;
+}
+
+void copy(char to[], char from[]) {
+  int i = 0;
+
+  while((to[i] = from[i]) != '\0')
+    ++i;
+}
