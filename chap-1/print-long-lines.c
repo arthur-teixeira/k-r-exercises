e1fdce82683f07e642043df0240e27fde73c4526
arthur.teixeira <arthurcarvalho24@gmail.com>
2022-12-28 21:02:00 -0300

    Simpler version, prints directly without result matrix

--- chap-1/print-long-lines.c
+++ chap-1/print-long-lines.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 
 #define MAX_LINE_SIZE 200
-#define MAX_LINES 10
 
 int get_next_line(char line[], int maxline);
 void copy(char to[], char from[]);
 
 int main() {
-  int len, big_line_count;
-  char big_lines[MAX_LINES][MAX_LINE_SIZE];
+  int len;
   char cur_line[MAX_LINE_SIZE];
 
-  big_line_count = 0;
   for (;;) {
     len = get_next_line(cur_line, MAX_LINE_SIZE);
     if (len == 0) break;
     if (len >= 80) {
-      copy(big_lines[big_line_count], cur_line);
-      ++big_line_count;
+      printf("%d characters long: %s\n", len, cur_line);
     } 
   }
-
-  for(int i =0; i < big_line_count; ++i)
-    printf("%d %s\n", i, big_lines[i]);
 
   return 0;
 }
@@ -41,13 +34,4 @@ int get_next_line(char s[], int lim) {
   s[i] = '\0';
 
   return i;
-}
-
-void copy(char to[], char from[]) {
-  int i = 0;
-
-  while(from[i] != '\0') {
-    to[i] = from[i];
-    ++i;
-  }
 }
