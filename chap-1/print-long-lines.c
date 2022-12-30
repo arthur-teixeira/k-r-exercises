c368abb4dabd490b845dd8a96f7e633983b391de
arthur-teixeira <arthurcarvalho24@gmail.com>
2022-12-30 19:27:36 -0300

    Refactoring

--- chap-1/print-long-lines.c
+++ chap-1/print-long-lines.c
@@ -3,15 +3,14 @@
 #define MAX_LINE_SIZE 200
 #define LINE_SIZE_BREAKPOINT 80
 
-int get_next_line(char line[], int maxline);
-void copy(char to[], char from[]);
+int get_line(char line[], int maxline);
 
 int main() {
   int len;
   char cur_line[MAX_LINE_SIZE];
 
   for (;;) {
-    len = get_next_line(cur_line, MAX_LINE_SIZE);
+    len = get_line(cur_line, MAX_LINE_SIZE);
     if (len == 0) break;
     if (len >= LINE_SIZE_BREAKPOINT) {
       printf("%d characters long: %s\n", len, cur_line);
@@ -21,15 +20,16 @@ int main() {
   return 0;
 }
 
-int get_next_line(char s[], int lim) {
+int get_line(char s[], int lim) {
   int c, i;
 
   i = 0;
   while (i < lim - 1) {
     c = getchar();
-    if (c == EOF || c == '\n') break;
-    s[i] = c;
-    ++i;
+    if (c != EOF && c != '\n') {
+      s[i] = c;
+      ++i;
+    };
   }
 
   s[i] = '\0';
