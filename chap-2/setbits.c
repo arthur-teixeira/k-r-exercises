9a78ab48b27c9172a8bc4ab50b7724b0e51e6ced
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-02-22 19:12:47 -0300

    2-6 setbits with step by step explanation

---
+++ chap-2/setbits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int setbits(int x, int p, int n, int y);
+int main() {
+    const int result = setbits(218, 3, 3, 170);
+    printf("result is %d\n", result);
+}
+
+int setbits(int x, int p, int n, int y) {
+    int bitmask = ~0; // 1111 1111
+    bitmask = bitmask << n; //1111 1000, shifts the mask to get n zeros at the end
+    bitmask = ~bitmask; //0000 0111, inverts to get correct mask
+
+    y = bitmask & y; //gets rightmost n bits in y
+    y = y << p; //shifts y by p bits to place it in correct position
+
+    bitmask = bitmask << p; //shifts bitmask by p to place it in correct position to mask x
+    bitmask = ~bitmask; //inverts mask to get correct bits from x
+    x = bitmask & x; //gets other bits fom x
+    return y | x; //merge;
+}
