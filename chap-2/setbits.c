f0de8f9691db84a3cd8766aa1b3be50257e7b18f
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-02-23 20:04:19 -0300

    Refactor to use assignment operators

--- chap-2/setbits.c
+++ chap-2/setbits.c
@@ -8,14 +8,14 @@ int main() {
 
 int setbits(int x, int p, int n, int y) {
     int bitmask = ~0; // 1111 1111
-    bitmask = bitmask << n; //1111 1000, shifts the mask to get n zeros at the end
+    bitmask <<= n; //1111 1000, shifts the mask to get n zeros at the end
     bitmask = ~bitmask; //0000 0111, inverts to get correct mask
 
-    y = bitmask & y; //gets rightmost n bits in y
-    y = y << p; //shifts y by p bits to place it in correct position
+    y &= bitmask; //gets rightmost n bits in y
+    y <<= p; //shifts y by p bits to place it in correct position
 
-    bitmask = bitmask << p; //shifts bitmask by p to place it in correct position to mask x
+    bitmask <<= p; //shifts bitmask by p to place it in correct position to mask x
     bitmask = ~bitmask; //inverts mask to get correct bits from x
-    x = bitmask & x; //gets other bits fom x
+    x &= x; //gets other bits fom x
     return y | x; //merge;
 }
